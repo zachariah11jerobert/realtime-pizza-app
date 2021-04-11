290f8e7c3578f312be138bfaf7212684ada859af
unknown <zachariah11jerobert@gmail.com>
2021-04-11 09:53:15 +0530

    add to cart functionlity done

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -1,7 +1,10 @@
+const Menu=require('../../models/menu');
 function homeController() {
   return {
-    index(req, res) {
-      res.render("home");
+    async index(req, res) {
+      const pizzas=await Menu.find()
+     // console.log(pizzas)
+      return res.render('home',{pizzas:pizzas})
     },
   };
 }
