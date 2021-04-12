572cb67b3966bb76121ad9fa081ad19cc48b560b
unknown <zachariah11jerobert@gmail.com>
2021-04-12 13:17:40 +0530

    auth system

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("express-flash");
 const MongoDbStore = require("connect-mongo")(session);
+const passport = require("passport");
 
 // Database connection
 const url = "mongodb://localhost/pizza";
@@ -45,14 +46,22 @@ app.use(
   })
 );
 
+// Passport config
+const passportInit = require("./app/config/passport");
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 
 // Assets
 app.use(express.static("public"));
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 // Global middleware
 app.use((req, res, next) => {
   res.locals.session = req.session;
+  res.locals.user = req.user;
   next();
 });
 
