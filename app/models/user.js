671dba43ca854e59b824ef416d96cea0539f2adf
unknown <zachariah11jerobert@gmail.com>
2021-08-13 16:05:53 +0530

    1.re uploaded code

--- app/models/user.js
+++ app/models/user.js
@@ -6,6 +6,6 @@ const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, default: "customer" },
-});
+},{timestamps:true});
 
 module.exports = mongoose.model("User", userSchema);
